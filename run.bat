d9379c95ddc27022096131459a986087e17057f4
溪水娃娃 <51937188+syfolen@users.noreply.github.com>
2024-09-24 00:11:29 +0800

    阿彩三仙配速

--- run.bat
+++ run.bat
@@ -6,8 +6,8 @@ echo 若没有安装 nodejs 请自行前往 https://nodejs.p2hp.com/ 下载
 echo 一般下载带 LTS 的稳定版就行了
 
 node -v
-::node main.js 阿彩 盾 于吉,左慈,张角
+node main.js 阿彩 盾 于吉,左慈,张角
 ::node main.js 落晚霞 枪 SP诸葛亮,张苞,关兴
-node main.js 落晚霞 盾 于吉,左慈-魏,张角
+::node main.js 落晚霞 盾 于吉,左慈-魏,张角
 
 @pause
